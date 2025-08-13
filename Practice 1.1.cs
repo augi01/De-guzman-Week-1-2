69e821cf051a7154718d7a494035f351f9950da9
Augi01 <augifrancodeguzman01@gmail.com>
2025-08-13 13:23:59 +0800

    Update and rename Practice 1.1 to Practice 1.1.cs

---
+++ Practice 1.1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      //Practice 01.1
+      
+        int num = 8;
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("The number " + num + " is even.");
+        }
+    }
+}
