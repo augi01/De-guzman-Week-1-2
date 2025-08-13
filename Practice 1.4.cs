1f6ad040b5002375f05d8203689e9a0d98a8929b
Augi01 <augifrancodeguzman01@gmail.com>
2025-08-13 13:29:14 +0800

    Create Practice 1.4.cs

---
+++ Practice 1.4.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+       // Practice 1.4
+      
+        int scores = 45;
+        string result = (scores >= 50) ? "Passed" : "Failed";
+        Console.WriteLine(result);
+    }
+}
