dbb86b8f1f0d6ce835c4f48e6768305bdc01661c
Augi01 <augifrancodeguzman01@gmail.com>
2025-08-13 13:25:18 +0800

    Create And  Code Practice 1.2.cs

---
+++ Practice 1.2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      // Practice 01.2
+      
+        int num = -3;
+        if (num > 0)
+        {
+            Console.WriteLine("The number " + num + " is positive.");
+        }
+        else
+        {
+            Console.WriteLine("The number " + num + " is negative.");
+        }
+    }
+}
